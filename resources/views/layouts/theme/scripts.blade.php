6e43f03e6d534c3a3a9fac3a3d324a346ed61993
cleverbe <cleverbernal123@gmail.com>
2021-11-12 15:13:47 -0400

    nuevo1

---
+++ resources/views/layouts/theme/scripts.blade.php
@@ -0,0 +1,36 @@
+<script src="{{ asset('assets/js/libs/jquery-3.1.1.min.js') }}"></script>
+<script src=bootstrap/js/popper.min.js></script>
+<script src=bootstrap/js/bootstrap.min.js></script>
+<script src="{{ asset('plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        App.init();
+    });
+</script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+
+<script src="{{ asset('plugins/sweetalerts/sweetalerts2.min.js') }}"></script>
+<script src="{{ asset('plugins/notificaction/snackbar/snackbar.min.js') }}"></script>
+<script src="{{ asset('plugins/nicescroll/nicescroll.min.js') }}"></script>
+<script src="{{ asset('plugins/currency/currency.js') }}"></script>
+<script>
+    function noty(msg, option = 1) {
+        Snackbar.show({
+            text: msg.toUpperCase(),
+            actionText: 'Cerrar Exit',
+            actionTextColor: '#FFFFFF',
+            backgroungColor: option == 1 ? '#FF7900' : '#e7515a',
+            pos: 'top-right'
+        });
+    }
+</script>
+
+@livewireScripts
+
+<!-- END GLOBAL MANDATORY SCRIPTS -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+
+<!-- <script src="{{ asset('plugins/apex/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/js/dashboard/dash_1.js') }}"></script> -->
---
+++ resources/views/layouts/theme/styles.blade.php
@@ -0,0 +1,56 @@
+<script src="{{ asset('assets/js/loader.js') }}"></script>
+<link href="{{ asset('assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
+
+<link href="{{ asset('plugins/apex/apexcharts.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('assets/css/dashboard/dash_1.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('plugins/font-icons/fontawesome/css/fontawesome.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('assets/css/elements/avatar.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('plugins/sweetalerts/sweetalert.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('plugins/notification/snackbar/snackbar.min.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('assets/css/widgets/modules-widgets.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/forms/theme-checkbox-radio.css') }}" rel="stylesheet" type="text/css" />
+
+<style>
+    aside {
+        display: none !important;
+    }
+
+    .page-item.active .page-link {
+        z-index: 3;
+        color: #ffffff;
+        background-color: #3b3f5c;
+        border-color: #3b3f5c;
+    }
+
+    @media (max-width:480px) {
+        .mtmobile {
+            margin-bottom: 20px !important;
+        }
+
+        .mbmobile {
+            margin-bottom: 10px !important;
+        }
+
+        .hideonm {
+            display: none !important;
+        }
+
+        .indblock {
+            display: block;
+        }
+    }
+</style>
+
+
+@livewireStyles
