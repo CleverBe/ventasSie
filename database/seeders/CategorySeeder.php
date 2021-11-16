325f710f883ffaff4ad96dd00e0dff9a68e080ee
cleverbe <cleverbernal123@gmail.com>
2021-11-16 14:56:24 -0400

    sinfactories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,24 +14,6 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        Category::create([
-            'name'=>'CURSOS',
-            'image'=>'https://dummyimage.com/200x150/f516f5/0a0a0a'
-        ]);
-        Category::create([
-            'name'=>'TENIS',
-            'image'=>'https://dummyimage.com/200x150/f516f5/0a0a0a'
-        ]);
-        Category::create([
-            'name'=>'CELULARES',
-            'image'=>'https://dummyimage.com/200x150/f516f5/0a0a0a'
-        ]);
-        Category::create([
-            'name'=>'COMPUTADORAS',
-            'image'=>'https://dummyimage.com/200x150/f516f5/0a0a0a'
-        ]);
-=======
         Category::Create([
             'name'=>'CURSOS',
             'image'=>'https://dummyimage.com/200x150/ff9a03/ffffff'
@@ -49,6 +31,5 @@ class CategorySeeder extends Seeder
             'image'=>'https://dummyimage.com/200x150/ff9a03/ffffff'
         ]);
         
->>>>>>> f8804e3338d4b24ea8f205abcb9e454d603e2bd7
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-<<<<<<< HEAD
-use Database\Factories\CategoryFactory;
-=======
->>>>>>> f8804e3338d4b24ea8f205abcb9e454d603e2bd7
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,20 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        // \App\Models\User::fctory(10)->create();
-=======
         // \App\Models\User::factory(10)->create();
         //Category::factory(20)->create();
->>>>>>> f8804e3338d4b24ea8f205abcb9e454d603e2bd7
         $this->call(DenominationSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(UserSeeder::class);
-<<<<<<< HEAD
-
-       /*  Category::factory(20)->create(); */
-=======
->>>>>>> f8804e3338d4b24ea8f205abcb9e454d603e2bd7
     }
 }
