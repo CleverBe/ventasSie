31ea3ab5c436e9556e2a31a4b3a8510477a90ccb
Fenris <h666heavymetal@gmail.com>
2021-11-16 14:49:59 -0400

    merged martes

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,7 +14,6 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         Category::create([
             'name'=>'CURSOS',
             'image'=>'https://dummyimage.com/200x150/f516f5/0a0a0a'
@@ -31,24 +30,5 @@ class CategorySeeder extends Seeder
             'name'=>'COMPUTADORAS',
             'image'=>'https://dummyimage.com/200x150/f516f5/0a0a0a'
         ]);
-=======
-        Category::Create([
-            'name'=>'CURSOS',
-            'image'=>'https://dummyimage.com/200x150/ff9a03/ffffff'
-        ]);
-        Category::Create([
-            'name'=>'TENIS',
-            'image'=>'https://dummyimage.com/200x150/ff9a03/ffffff'
-        ]);
-        Category::Create([
-            'name'=>'CELULARES',
-            'image'=>'https://dummyimage.com/200x150/ff9a03/ffffff'
-        ]);
-        Category::Create([
-            'name'=>'COMPUTADORAS',
-            'image'=>'https://dummyimage.com/200x150/ff9a03/ffffff'
-        ]);
-        
->>>>>>> f8804e3338d4b24ea8f205abcb9e454d603e2bd7
     }
 }
