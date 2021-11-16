3b713fb2fb7ecf1409fdcf4f200045a9cc525f55
cleverbe <cleverbernal123@gmail.com>
2021-11-16 18:36:11 -0400

    tablas-fin-martes

---
+++ resources/views/livewire/startpage/init.php
@@ -0,0 +1,59 @@
+<div class="row sales layout-top-spacing">
+<div class="col-sm-12">
+    <div class="widget widget-chart-one">
+        <div class="widget-heading">
+            <h4 class="card-title">
+                <b>ComponentName | PageTitle</b>
+            </h4>
+            <ul class="tabs-pills">
+                <li>
+                    <a href="javascript:void(0)"class="tabmenu bg-dark" data-toggle="modal" data-target=
+                    "#themodal">Agregar</a>
+                </li>
+            </ul>
+        </div>
+        Search
+
+        <div class="widget-content">
+            <div class="table-responsive">
+                <table class="table table-bordered table striped mt-1">
+                    <thead class="text-white" style="background:#3B3F5C">
+                <TR>
+                    <th class="table-th text-white">DESCRIPCION</th>
+                    <th class="table-th text-white">IMAGEN</th>
+                    <th class="table-th text-white">ACCIONES</th>
+                </TR>
+                </thead>
+                <Tbody>
+                    <tr>
+                        <td><h6>Nombre de Categoría</h6></td>
+                        <td class="text-center">
+                            <span>
+                                <img src="" alt="imagen de ejemplo" height="70" width="80" class="rounded">
+
+                            </span>
+                        </td>
+                        <td class="text-center">
+                            <a href="javaescript:void(0)" class="btn btn-dark mtmobile" title="Editar">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <a href="javaescript:void(0)" class="btn btn-dark " title="Eliminar">
+                                <i class="fas fa-trash"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </Tbody>
+
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
+Include Form
+</div>
+<SCRipt>
+    document.addEventListener('DOMContentLoader',function(){
+
+    });
+</SCRipt>
