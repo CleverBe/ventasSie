95fafe3ee33d5886669aaa0e0455aefd997fd7c2
Fenris <h666heavymetal@gmail.com>
2021-11-18 14:56:42 -0400

    init.php plantilla de lso content

---
+++ resources/views/livewire/startpage/init.php
@@ -0,0 +1,56 @@
+<div class="row sales layout-top-spacing">
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+            <div class="widget-heading">
+                <h4 class="card-title">
+                    <b>ComponentName | PageTitle</b>
+                </h4>
+                <ul class="tabs tab-pills">
+                    <li>
+                        <a href="javascript:void(0)" class="tabmenu bg-dark" data-toggle="modal" data-target="#themodal">Agregar</a>
+                    </li>
+                </ul>
+            </div>
+            Search
+
+            <div class="widget-content">
+                <div class="table-responsive">
+                    <table class="table table-bordered table striped mt-1">
+                        <thead class="text-white" style="background: #3B3F5C">
+                            <tr>
+                                <th class="table-th text-withe">DESCRIPCIÓN</th>
+                                <th class="table-th text-withe">IMAGEN</th>
+                                <th class="table-th text-w  ithe">ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><h6>Nombre Categoría</h6></td>
+                                <td class="text-center">
+                                    <span>
+                                        <img src="" alt="imagen de ejemplo" height="70" width="80" class="rounded">
+                                    </span>
+                                </td>
+                                <td class="text-center">
+                                    <a href="javascript:coid(0)" class="btn btn-dark mtmobile" title="Editar">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="javascript:coid(0)" class="btn btn-dark" title="Eliminar">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    Pagination
+                </div>
+            </div>
+        </div>
+    </div>
+
+    Include Form
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function(){});
+</script>
